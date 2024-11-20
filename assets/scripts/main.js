2a83fafb97c0a3874f10f9f6e444954304bf453a
Dacardonac <dacardonac201@gmail.com>
2024-11-19 20:43:50 -0500

    feat: implement SweetAlert2 for user notifications

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,52 +1,65 @@
+import Swal from 'sweetalert2';
+
 let amount = document.getElementById('amount');
 let btnGenerator = document.getElementById('generator');
 let btnReset = document.getElementById('reset');
 let btnCopy = document.getElementById('copy');
 let inputPassword = document.getElementById('password');
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
-
-
+const characters =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
 
 function genaratePassword() {
   let number = parseInt(amount.value);
-
-  if (number < 8) {
-    alert('The password must be 8 or more characters');
-    return;
-  };
-
   let password = '';
 
   for (let i = 0; i < number; i++) {
     let randomCharacter = Math.floor(Math.random() * characters.length);
     password += characters[randomCharacter];
-  };
+  }
   inputPassword.value = password;
-};
+}
 
 btnGenerator.addEventListener('click', genaratePassword);
 
-function reset() {
-  let userResponse = confirm('Are you sure you want to delete the generated password?');
-  if (userResponse) {
-    inputPassword.value = '';
-  }
-  else {
+async function reset() {
+  if (inputPassword.value === '') {
+    await Swal.fire('There is no text to delete');
     return;
-  };
-};
+  }
+
+  let result = await Swal.fire({
+    title: 'Are you sure?',
+    text: 'Are you sure you want to Delete the Generated Password?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#06ab00',
+    cancelButtonColor: '#ad0303',
+    confirmButtonText: 'Yes, delete it!',
+  });
+  if (result.isConfirmed) {
+    inputPassword.value = '';
+  } else {
+    return;
+  }
+}
 
 btnReset.addEventListener('click', reset);
 
-function copyText() {
+async function copyText() {
   if (inputPassword.value !== '') {
     inputPassword.select();
     navigator.clipboard.writeText(inputPassword.value);
-    alert('Text Copied Successfully');
+    await Swal.fire({
+      position: 'top-end',
+      width: '30rem',
+      icon: 'success',
+      title: 'The Password was Copied Successfully',
+      showConfirmButton: false,
+      timer: 2500,
+    });
+  } else {
+    await Swal.fire('There is no text to Copy');
   }
-  else {
-    alert('There is no text to Copy');
-  };
 }
 
 btnCopy.addEventListener('click', copyText);
