3d176756eaf520f4e877be89e5fb5803f0da313d
Dacardonac <dacardonac201@gmail.com>
2024-11-19 19:31:57 -0500

    feat: add copy functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,7 @@
 let amount = document.getElementById('amount');
 let btnGenerator = document.getElementById('generator');
 let btnReset = document.getElementById('reset');
+let btnCopy = document.getElementById('copy');
 let inputPassword = document.getElementById('password');
 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
 
@@ -25,13 +26,27 @@ function genaratePassword() {
 
 btnGenerator.addEventListener('click', genaratePassword);
 
-btnReset.addEventListener('click', () => {
+function reset() {
   let userResponse = confirm('Are you sure you want to delete the generated password?');
-
   if (userResponse) {
     inputPassword.value = '';
   }
   else {
-    return
+    return;
+  };
+};
+
+btnReset.addEventListener('click', reset);
+
+function copyText() {
+  if (inputPassword.value !== '') {
+    inputPassword.select();
+    navigator.clipboard.writeText(inputPassword.value);
+    alert('Text Copied Successfully');
   }
-});
+  else {
+    alert('There is no text to Copy');
+  };
+}
+
+btnCopy.addEventListener('click', copyText);
